dcb32ea959b98feea1a2f54323d6f39dac9d148d
levg34 <levg34@users.noreply.github.com>
2024-05-03 19:57:49 +0000

    add global styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import '~/styles.css'
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
