9b26fb2e57f98ff6c64b0e526cf28981d4dc3ea8
levg34 <levg34@users.noreply.github.com>
2024-05-03 20:19:25 +0000

    add missing sections

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,17 @@
 import type { MetaFunction } from '@remix-run/node'
 import Bio from '~/components/Bio'
 import Carousel from '~/components/Carousel'
+import Composition from '~/components/Composition'
+import Concerts from '~/components/Concerts'
+import Contact from '~/components/Contact'
 import Footer from '~/components/Footer'
 import Links from '~/components/Links'
 import Music from '~/components/Music'
 import Navbar from '~/components/Navbar'
 import Newsletter from '~/components/Newletter'
+import Repertoire from '~/components/Repertoire'
+import Studies from '~/components/Studies'
+import Videos from '~/components/Videos'
 
 export const meta: MetaFunction = () => {
     return [
@@ -20,12 +26,12 @@ export default function Index() {
             <Navbar />
             <Carousel />
             <Bio />
-            {/* Studies */}
-            {/* Concerts */}
-            {/* Repertoire */}
-            {/* Composition */}
-            {/* Videos */}
-            {/* Contact */}
+            <Studies />
+            <Concerts />
+            <Repertoire />
+            <Composition />
+            <Videos />
+            <Contact />
             <Newsletter />
             <Links />
             <Music />
