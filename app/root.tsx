98b81be7cabdabeeb225147e4f4c17a7928ba7d7
levg34 <levg34@users.noreply.github.com>
2024-05-06 19:46:21 +0000

    add jQuery and bootstrap js

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 ></link>
                 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css"></link>
                 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css"></link>
+                <script
+                    src="https://code.jquery.com/jquery-1.12.4.min.js"
+                    integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ"
+                    crossOrigin="anonymous"
+                ></script>
+                <script
+                    src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"
+                    integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
+                    crossOrigin="anonymous"
+                ></script>
             </head>
             <body>
                 {children}
