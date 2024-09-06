01897f9d8d6f6258b0ace350b17ce10bfb2f0871
levg34 <levg34@users.noreply.github.com>
2024-09-06 10:17:36 +0000

    change hardcoded URL

--- app/components/common/NicoImg.tsx
+++ app/components/common/NicoImg.tsx
@@ -1,7 +1,7 @@
 const NicoImg = () => (
     <div className="container">
         <img
-            src="https://nicolasdross.fr/uploads/9ef7bfc25ab4b8893422ed97659f931a"
+            src="https://api.nicolasdross.fr/uploads/9ef7bfc25ab4b8893422ed97659f931a"
             alt="Nicolas Pianiste"
             style={{ maxWidth: '100%', marginBottom: '5px' }}
         />
