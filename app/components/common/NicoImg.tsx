288b16cc1fc6bf6238976082d369ef3bfceffb8d
levg34 <levg34@users.noreply.github.com>
2024-12-24 18:21:36 +0000

    repair img on error pages

--- app/components/common/NicoImg.tsx
+++ app/components/common/NicoImg.tsx
@@ -1,7 +1,9 @@
+import { IMG_URL } from '~/ts/constants'
+
 const NicoImg = () => (
     <div className="container">
         <img
-            src="https://api.nicolasdross.fr/uploads/9ef7bfc25ab4b8893422ed97659f931a"
+            src={IMG_URL + '/uploads/9ef7bfc25ab4b8893422ed97659f931a'}
             alt="Nicolas Pianiste"
             style={{ maxWidth: '100%', marginBottom: '5px' }}
         />
--- app/ts/constants.ts
+++ app/ts/constants.ts
@@ -1,1 +1,2 @@
 export const ACTION_STRING = '_action'
+export const IMG_URL = process.env.BACKEND_URL
